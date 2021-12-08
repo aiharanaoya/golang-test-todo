49594d6550ce60640acca44b18b2ea1b8687d38c
相原 直也 <aiharanaoya@team-lab.com>
2021-12-08 19:47:19 +0900

    add: main.go

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/aiharanaoya/golang-test-todo
+
+go 1.16
+
+require gopkg.in/go-ini/ini.v1 v1.66.2
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/aiharanaoya/golang-test-todo/config"
+)
+
+func main() {
+	fmt.Println(config.Config.Port)
+	fmt.Println(config.Config.SQLDriver)
+	fmt.Println(config.Config.DbName)
+	fmt.Println(config.Config.LogFile)
+}
